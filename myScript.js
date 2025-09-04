29fd74c5b2e5ac6d8cbad39ecf60670fdd7148e1
AvailableUsername124 <kaylebhanacek@gmail.com>
2025-09-04 14:48:45 -0700

    Feat: check reveal 1 answer

--- myScript.js
+++ myScript.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-	$(".disappear").hide();
-	$("button").click(function() {
-		$(".disappear").show();
+	$("#answ1").hide();
+	$("#check1").click(function() {
+		$("#answ1").toggle();
+	});
+	$("#answ2").hide();
+	$("#check2").click(function() {
+		$("#answ2").toggle();
+	});
+	$("#answ3").hide();
+	$("#check3").click(function() {
+		$("#answ3").toggle();
+	});
+	$("#answ4").hide();
+	$("#check4").click(function() {
+		$("#answ4").toggle();
 	});
 });
 
