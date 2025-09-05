25abf62eb9f04b62c64f75c16f373c70cc4edb1a
AvailableUsername124 <kaylebhanacek@gmail.com>
2025-09-05 13:24:02 -0700

    Feat: add press enter tell answer

--- myScript.js
+++ myScript.js
@@ -1,20 +1,50 @@
 $(document).ready(function() {
 	$("#answ1").hide();
 	$("#check1").click(function() {
-		$("#answ1").toggle();
+		$("#answ1").show();
 	});
 	$("#answ2").hide();
 	$("#check2").click(function() {
-		$("#answ2").toggle();
+		$("#answ2").show();
 	});
 	$("#answ3").hide();
 	$("#check3").click(function() {
-		$("#answ3").toggle();
+		$("#answ3").show();
 	});
 	$("#answ4").hide();
 	$("#check4").click(function() {
-		$("#answ4").toggle();
+		$("#answ4").show();
 	});
+
+var input = document.getElementById("answer box1");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("check1").click();
+  }
+});
+var input = document.getElementById("answer box2");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("check2").click();
+  }
+});
+var input = document.getElementById("answer box3");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("check3").click();
+  }
+});
+var input = document.getElementById("answer box4");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("check4").click();
+  }
+});
+
 });
 
 	
